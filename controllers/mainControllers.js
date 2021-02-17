484b7bb0774dd7bfa15b181fe9124bbcb2aab441
Robertlean <robert_lean@live.com.ar>
2021-02-17 17:54:50 -0300

    mudando todos las vistas

--- controllers/mainControllers.js
+++ controllers/mainControllers.js
@@ -4,5 +4,35 @@ module.exports = {
     },
     createpost: (req, res, next) => {
         res.render('createpost', {title: 'Crear Post', css: 'estilos.css'})
-    }
+    },
+    post: (req, res, next) =>{
+        res.render('post', {title: 'Post', css:'estilos.css'})
+    },
+    aboutme: (req, res, next) =>{
+        res.render('aboutme', {title: 'Sobre mí', css:'estilos.css'})
+    },
+    buy: (req, res, next) =>{
+        res.render('buy', {title: 'Comprar Sesion', css:'estilos.css'})
+    },
+    experience: (req, res, next) =>{
+        res.render('experienceclient', {title: 'Experiencia', css:'estilos.css'})
+    },
+    login: (req, res, next) =>{
+        res.render('login', {title: 'Iniciar sesión', css:'estilos.css'})
+    },
+    user: (req, res, next) =>{
+        res.render('perfil', {title: 'Tu perfil', css:'estilos.css'})
+    },
+    search: (req, res, next) =>{
+        res.render('search', {title: 'Resultado de la busqueda', css:'estilos.css'})
+    },
+    service: (req, res, next) =>{
+        res.render('service', {title: 'Sesiones', css:'estilos.css'})
+    },
+    register: (req, res, next) =>{
+        res.render('register', {title: 'Registrate', css:'estilos.css'})
+    },
+    horoscopo: (req, res, next) =>{
+        res.render('horoscopo', {title: 'Horoscopo', css:'estilos.css'})
+    },
 }
--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,16 @@ var router = express.Router();
 var controller = require('../controllers/mainControllers')
 /* GET home page. */
 router.get('/', controller.index);
-router.get('/createpost', controller.createpost)
+router.get('/createpost', controller.createpost);
+router.get('/post', controller.post);
+router.get('/aboutme', controller.aboutme);
+router.get('/buy', controller.buy);
+router.get('/experienceclient', controller.experience);
+router.get('/login', controller.login);
+router.get('/perfil', controller.user);
+router.get('/search', controller.search);
+router.get('/service', controller.service);
+router.get('/register', controller.register);
+router.get('/horoscopo', controller.horoscopo);
 
 module.exports = router;
