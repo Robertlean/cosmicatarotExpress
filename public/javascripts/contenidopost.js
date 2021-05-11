14da9867304af769d083aa3db3411348ac6f6f64
Robertlean <robert_lean@live.com.ar>
2021-05-11 08:23:14 -0300

    usando DOM

--- public/javascripts/contenidopost.js
+++ public/javascripts/contenidopost.js
@@ -1,8 +1,8 @@
-var cantclick = 0;/*
+var cantclick = 0;
 document.getElementById("imgposteo").addEventListener("click", function( event ) {
     // presentar la cuenta de clicks realizados sobre el elemento con id "prueba"
     cantclick++;
     if(cantclick < 10){      
       console.log(cantclick)
     }
-  }, false);*/
+  }, false);
